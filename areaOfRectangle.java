141939ce230a901f446b79f64d872cfef4b70335
AnkitaDudhe1910 <121152074+AnkitaDudhe1910@users.noreply.github.com>
2023-01-23 16:14:07 +0530

    area of rectangle

---
+++ areaOfRectangle.java
@@ -0,0 +1,10 @@
+class areaOfRectangle
+{
+public static void main(String[]args)
+ {
+  int length=12, breadth=15;
+  int area;
+  area = length*breadth;
+  System.out.println("area of rectangle is:"+ area);
+ }
+}
