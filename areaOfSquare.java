ea2bd030657bbefc9479a971ac2a4dda811338fe
AnkitaDudhe1910 <121152074+AnkitaDudhe1910@users.noreply.github.com>
2023-01-25 10:27:50 +0530

    area of square

---
+++ areaOfSquare.java
@@ -0,0 +1,10 @@
+class areaOfSquare
+{
+ public static void main(String[] args)
+ {
+   int length=4, breadth=4;
+   int area;
+   area = length*breadth;
+   System.out.println("area of square is:"+area);
+ }
+}
