4cb451669dc3b6de0b23c4518833b1ec14fc6317
AnkitaDudhe1910 <121152074+AnkitaDudhe1910@users.noreply.github.com>
2023-01-25 10:28:20 +0530

    area of triangle

---
+++ areaOfTriangle.java
@@ -0,0 +1,10 @@
+class areaOfTriangle
+{
+public static void main(String [] args)
+{
+int length=4, breadth=6,height=8;
+int area;
+area = length*breadth*height;
+System.out.println("area of triangle is: "+area);
+}
+}
