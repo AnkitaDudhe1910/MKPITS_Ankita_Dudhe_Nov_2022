35a9701f9e530598fe9086f4089c8b810ef356fa
Ankita Dudhe <ankitadudhe1996@gmail.com>
2023-01-25 12:54:43 +0530

    multiplication

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+class Multiplication
+{
+public static void main(String [] args)
+{
+  int a = 5 , b=8 , res ;
+  res= a*b;
+  System.out.println("multiplication of two number is : "+res);
+
+ }
+ }
