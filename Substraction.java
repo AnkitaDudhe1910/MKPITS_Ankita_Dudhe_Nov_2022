90603f3a45647fcd3f90b1cf2b30651d42ae37f2
Ankita Dudhe <ankitadudhe1996@gmail.com>
2023-01-25 13:03:08 +0530

    substraction

---
+++ Substraction.java
@@ -0,0 +1,9 @@
+class Substraction
+{
+  public static void main(String [] args)
+  {
+  int a = 50, b=30, res;
+  res = a - b;
+  System.out.println("Substraction of two number is : "+res);
+  }
+  }
