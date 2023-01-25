e69b175f587e1f0750ef1205dc708353d2c60c77
Ankita Dudhe <ankitadudhe1996@gmail.com>
2023-01-25 12:23:21 +0530

    circumference of circle

---
+++ circumOfCircle.java
@@ -0,0 +1,9 @@
+class circumOfCircle
+{
+public static void main(String[] args){
+  float radius =7.0f;
+ double circum ;
+  circum = 2*3.14*radius;
+  System.out.println("circumference of circle is:"+circum);
+ }
+}
