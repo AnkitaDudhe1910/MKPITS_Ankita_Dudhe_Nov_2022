6b093493263125d0509dfade3846c918724646b1
Ankita Dudhe <ankitadudhe1996@gmail.com>
2023-01-25 13:05:54 +0530

    typecasting example

---
+++ Typecasting.java
@@ -0,0 +1,12 @@
+class Typecasting
+{
+public static void main(String [] args)
+{
+ byte a =4;
+ short s =a; //Implicit byte toshort
+ long l1 =14;
+ a =(byte)11; // Explicit long to byte
+ System.out.println(s);
+ System.out.println(a);
+ }
+ }
