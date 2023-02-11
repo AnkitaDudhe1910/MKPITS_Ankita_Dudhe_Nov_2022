bb184dcb5a0d3d85c15d4973869e3e13e0505356
Ankita Dudhe <ankitadudhe1996@gmail.com>
2023-02-11 07:34:41 +0530

    19th Jan Assignment

---
+++ Program_2_19.java
@@ -0,0 +1,22 @@
+class assignment
+{
+ void fun()
+ {
+    int res1 = (-1)+4*6;
+    int res2 = (35+5)%7;
+    int res3 = 14+(-4)*6/11;
+    int res4 = 2+15/6*1-7%2;
+    System.out.println("Result of first Expression is: "+res1);
+    System.out.println("Result of second Expression is: "+res2);
+    System.out.println("Result of third Expression is: "+res3);
+    System.out.println("Result of fourth Expression is: "+res4);
+ }
+}
+class Program_2_19
+{
+public static void main(String[] args)
+{
+assignment ob = new assignment();
+ob.fun();
+}
+}
